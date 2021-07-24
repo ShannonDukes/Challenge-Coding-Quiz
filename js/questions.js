6356bce3f36e6272d0df790417e6c4009f9d5392
Mrs.Dukes <mrs.knit.wit.dukes@gmail.com>
2021-07-23 22:06:28 -0700

    updates to formatting to ensure page be created

--- js/questions.js
+++ js/questions.js
@@ -1,6 +1,6 @@
 // creating an array and passing the number, questions, options, and answers
 let questions = [
-    {
+  {
     numb: 1,
     question: "What does HTML stand for?",
     answer: "Hyper Text Markup Language",
@@ -11,7 +11,7 @@ let questions = [
       "Hyper Tool Multi Language"
     ]
   },
-    {
+  {
     numb: 2,
     question: "What does CSS stand for?",
     answer: "Cascading Style Sheet",
@@ -22,7 +22,7 @@ let questions = [
       "Cascading Style Sheet"
     ]
   },
-    {
+  {
     numb: 3,
     question: "What does PHP stand for?",
     answer: "Hypertext Preprocessor",
@@ -33,7 +33,7 @@ let questions = [
       "Hometext Preprocessor"
     ]
   },
-    {
+  {
     numb: 4,
     question: "What does SQL stand for?",
     answer: "Structured Query Language",
@@ -44,7 +44,7 @@ let questions = [
       "Structured Query Language"
     ]
   },
-    {
+  {
     numb: 5,
     question: "What does XML stand for?",
     answer: "eXtensible Markup Language",
@@ -55,18 +55,4 @@ let questions = [
       "eXamine Multiple Language"
     ]
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
 ];
